63bcc2b3fcda202a23153f3bbe6ba7f311fe12cb
Neycker Aguayo <neyaldrichdev@outlook.com>
2017-09-25 01:39:16 -0500

    Sincronizando

--- restApi/serializers.py
+++ restApi/serializers.py
@@ -1,17 +1,29 @@
 from rest_framework import serializers
+<<<<<<< Updated upstream
 from restApi.models import Operador, Asistencia, Administrador
+=======
+from restApi.models import Operador, Asistencia, Admin
+
+class AdminSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Admin
+        fields = ('username', 'password')
+>>>>>>> Stashed changes
 
 class OperadorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Operador
-        fields = ('id', 'nombre', 'apellido', 'cedula', 'telefono', 'faceData')
+        fields = ('id', 'nombre', 'apellido', 'cedula', 'telefono', 'encodedFaceData')
 
 class AsistenciaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Asistencia
         fields = ('operador', 'latitud', 'longitud', 'fecha', 'hora', 'isEntrada')
+<<<<<<< Updated upstream
 
 class AdministradorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Administrador
         fields = ('username', 'password')
+=======
+>>>>>>> Stashed changes
